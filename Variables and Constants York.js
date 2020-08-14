a0d0be2f9bcb493c5c7746c43b2a88b3172b367f
York Cook <49798454+yorkcook@users.noreply.github.com>
2020-08-14 13:02:56 -0400

    Variables Assignment

---
+++ Variables and Constants York.js
@@ -0,0 +1,36 @@
+/*
+
+Var is used to assign values that can be updated. 
+Using var has some issues because the same variable name can be redeclared 
+inside of its scope 
+
+var myAge = 32
+
+*/
+
+/*
+Let is the new es6 way of declaring variables that you might want to update.
+The advantage let has over var is that while the value can be updated, the same variable name 
+can not be redeclared within the scope.
+
+let myCurrentTown = 'NYC'
+
+*/
+
+/*
+
+Const is used when you don't want a value to be updated. Unlike let or var const
+must be intialized when declared.
+
+const myName = 'York'
+
+*/
+
+
+/*
+
+Hoisting is when variable and function declarations are moved to the top 
+their scope before execution of the code, which allows variables to be used 
+before they are declared. 
+
+*/
